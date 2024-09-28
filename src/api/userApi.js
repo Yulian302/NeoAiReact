2067c0b89e54748d4644ae1f1ba6cb019c805786
Yulian302 <bohomolyulian3022003@gmail.com>
2024-09-28 12:31:30 -0500

    Redeploy

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,6 +1,6 @@
 // const API_BASE_URL = "https://sampledomain.space/api/";
 const API_BASE_URL = "https://neo-ai-backend-n5hw.onrender.com/api"
 
-const ML_SERVICE_BASE_URL = "https://api.sampledomain.space/"
+const ML_SERVICE_BASE_URL = "https://api.sampledomain.spacl/"
 
 export { API_BASE_URL, ML_SERVICE_BASE_URL }
