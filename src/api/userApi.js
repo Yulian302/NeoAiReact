34e82831939b7f485c5d734187a16a5dd8bc0ec2
Yulian302 <bohomolyulian3022003@gmail.com>
2024-09-30 16:43:32 -0500

    (depl): Fix api base url

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "https://sampledomain.space/api/";
-const API_BASE_URL = "https://neo-ai-backend-n5hw.onrender.com/api"
+const API_BASE_URL = "https://neo-ai-backend.onrender.com/api"
 
 const ML_SERVICE_BASE_URL = "https://api.sampledomain.spacl/"
 
