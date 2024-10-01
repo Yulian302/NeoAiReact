c7e6741b3a01a01e7b2cefe5b6aa7a4ce362634e
Yulian Bohomol <bohomolyulian3022003@gmail.com>
2024-10-01 16:36:27 -0500

    Update userApi.js

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://sampledomain.space/api/";
+const API_BASE_URL = "https://neo-ai-backend.onrender.com/api";
 
 const ML_SERVICE_BASE_URL = "https://api.sampledomain.space/";
 
