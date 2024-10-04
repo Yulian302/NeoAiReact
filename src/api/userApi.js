0d3cf72dfca78d580658c83785dc2abf0460e7f2
Yulian Bohomol <bohomolyulian3022003@gmail.com>
2024-10-04 14:53:09 -0500

    Update userApi.js

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,6 +1,6 @@
 
 const API_BASE_URL = "https://neo-ai-backend.onrender.com/api";
 
-const ML_SERVICE_BASE_URL = "https://neoai-fastapi.onrender.com"
+const ML_SERVICE_BASE_URL = "https://neoai-fastapi.onrender.com/"
 
 export { API_BASE_URL, ML_SERVICE_BASE_URL }
