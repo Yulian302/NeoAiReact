9bc638bcfc857444d44bbb3a88f7d5274e8adccb
Yulian302 <bohomolyulian3022003@gmail.com>
2024-10-04 14:54:04 -0500

    Resolve url bug

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,7 +1,5 @@
+const API_BASE_URL = "https://neo-ai-backend.onrender.com/api"
 
-
-const API_BASE_URL = "https://neo-ai-backend.onrender.com/api";
-
-const ML_SERVICE_BASE_URL = "https://neoai-fastapi.onrender.com"
+const ML_SERVICE_BASE_URL = "https://neoai-fastapi.onrender.com/"
 
 export { API_BASE_URL, ML_SERVICE_BASE_URL }
